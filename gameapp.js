f785d4bc41f2b68e3b0c1ccdd59af953f093ad62
cjpalacios07 <cjpalacios07@gmail.com>
2021-11-24 21:35:56 -0500

    Added Option to Play against CPU or userplus the checkers board and started working on Javascript variables for the pieces on the board

---
+++ gameapp.js
@@ -0,0 +1,8 @@
+document.getElementById("vscpu").onclick = function () {
+    location.href = "playcpu.html";
+}
+
+document.getElementById("vsuser").onclick = function (){
+    location.href = "playuser.html";
+}
+
